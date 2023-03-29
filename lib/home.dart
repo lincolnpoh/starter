f784242e87f2955502507c67706d27c29f71a976
lincolnpoh <lincoln_poh@outlook.com>
2023-03-29 15:39:54 +0800

    Done coding card3

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'card1.dart';
 import 'card2.dart';
+import 'card3.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -16,10 +17,7 @@ class _HomeState extends State<Home> {
   static List<Widget> pages = <Widget>[
     const Card1(), //Display Card1
     const Card2(), //Display Card2
-    //TODO: Replace with Card3
-    Container(
-      color: Colors.blue,
-    ),
+    const Card3(), //Display Card3
   ];
 
   void _onItemTapped(int index) {
